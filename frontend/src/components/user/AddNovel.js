16a6be033dcfafdb669f2121d1dfc5134476f217
tanukas1 <tanukashyap2019@gmail.com>
2022-05-21 23:54:48 +0530

    commit

--- frontend/src/components/user/AddNovel.js
+++ frontend/src/components/user/AddNovel.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-// import TextareaAutosize from '@mui/material/TextareaAutosize';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
